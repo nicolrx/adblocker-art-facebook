6f238038f4453aa7129a5cfc770ec343eecdc73f
nico_lrx <nicolas.leroux@sciencespo.fr>
2016-11-04 16:25:36 +0100

    Added for loop
    Extension works, need to add json images

--- content_script/script.js
+++ content_script/script.js
@@ -1,3 +1,18 @@
-var x = document.getElementsByClassName("_6b");
-console.log(x);
-x.style.display = "none";
+function addArt() {
+  var count = document.getElementsByClassName("uiStreamSponsoredLink").length;
+  console.log(count);
+
+  for (i = 0; i < count; i++) {
+    var x = document.getElementsByClassName("uiStreamSponsoredLink")[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+    x.className += " art";
+  }
+}
+
+addArt();
+window.onload = function () {
+  window.onscroll = function () {
+    if (document.body.scrollTop > 30) {
+      addArt();
+    }
+  };
+};
