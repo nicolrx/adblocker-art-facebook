214fff0b91e410ef39e60619a4898b289380dabb
nico_lrx <nicolas.leroux@sciencespo.fr>
2016-11-13 19:41:22 +0100

    Added side ads

--- content_script/script.js
+++ content_script/script.js
@@ -34,24 +34,41 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// Feed Ad
 function addArt() {
   var count = document.getElementsByClassName("uiStreamSponsoredLink").length;
   for (i = 0; i < count; i++) {
     var x = document.getElementsByClassName("uiStreamSponsoredLink")[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-      if (typeof x.style.content === 'undefined' || !x.style.content) {
+      if (typeof x.style.content === "undefined" || !x.style.content) {
         var random_img = getRandomInt(0,count_img);
         // x.className += "art".i;
         x.style.content = "url("+img[random_img]+")";
-        x.style.width = "500px";
+        x.style.width = "100%";
         x.style.cursor = "pointer";
         x.onclick = function () {
-          window.open('http://www.shimoni-illustration.com/','_blank');
+          window.open('http://www.shimoni-illustration.com/?ref=www.addart.co','_blank');
+        }
+      }
+    }
+
+  // Right Hand Side Ad
+  var count2 = document.getElementsByClassName("ego_section").length;
+  for (a = 0; a < count2; a++) {
+    var y = document.getElementsByClassName("ego_section")[a];
+      if (typeof y.style.content === "undefined" || !y.style.content) {
+        var random_img2 = getRandomInt(0,count_img);
+        // x.className += "art".i;
+        y.style.content = "url("+img[random_img2]+")";
+        y.style.width = "100%";
+        y.style.cursor = "pointer";
+        y.onclick = function () {
+          window.open('http://www.shimoni-illustration.com/?ref=www.addart.co','_blank');
         }
       }
     }
 };
 
-
+// call function
 addArt();
 window.onload = function () {
   window.onscroll = function () {
