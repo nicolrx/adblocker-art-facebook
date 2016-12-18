009838bd654363aea0d59272baa14e5f2e3df505
nico_lrx <nicolas.leroux@sciencespo.fr>
2016-12-18 14:44:26 +0100

    Update image rotations

--- content_script/script.js
+++ content_script/script.js
@@ -1,26 +1,165 @@
+var img = [  {  "artist": "Sara Andreasson",
+      "link": "http://cargocollective.com/saraandreasson",
+      "description": "Sara Andreasson is a Swedish illustrator based in London. She has come to be known through social media where she publishes her colorful, full of joy and energy drawings.",
+      "art" : ["http://www.partfaliaz.com/wp-content/uploads/2016/01/sara-andreasson4.jpg",
+        "http://www.partfaliaz.com/wp-content/uploads/2015/05/sara-andreasson2.jpg",
+        "http://payload379.cargocollective.com/1/2/76242/9898023/SaraAndreasson_WhiteNoise2_670.jpg",
+        "http://payload379.cargocollective.com/1/2/76242/9898023/SaraAndreasson_WhiteNoise1_670.jpg",
+        "http://payload379.cargocollective.com/1/2/76242/9898023/SaraAndreasson_WhiteNoise3_670.jpg",
+        "http://payload379.cargocollective.com/1/2/76242/9898023/SaraAndreasson_WhiteNoise5_670.jpg",
+        "http://payload379.cargocollective.com/1/2/76242/9898023/SaraAndreasson_WhiteNoise6_670.jpg",
+        "http://payload486.cargocollective.com/1/2/76242/12025212/SaraAndreasson_KathrynFerguson_Portrait_RGB_3_670.jpg",
+        "http://payload461.cargocollective.com/1/2/76242/11534993/SaraAndreasson_Tecknaren_Cover4_web_670.jpg",
+        "http://payload466.cargocollective.com/1/2/76242/11637429/SaraAndreasson_Riposte_1_web_670.jpg",
+        "http://payload463.cargocollective.com/1/2/76242/11568029/SaraAndreasson_LennyLetter_web_670.jpg",
+        "http://payload429.cargocollective.com/1/2/76242/10898100/prt_400x396_1451911629_2x.jpg",
+        "http://payload373.cargocollective.com/1/2/76242/9770448/SaraAndreasson_FashionIsMyPassion1_web_670.jpg",
+        "http://thesnobette.com/wp-content/uploads/2015/12/snobette-saraandreasson-art10.jpg",
+        "http://payload376.cargocollective.com/1/2/76242/9832973/SaraAndreasson_Sunblock_1_670.jpg",
+        "http://payload454.cargocollective.com/1/2/76242/11397366/doodle_670.jpg",
+        "http://payload480.cargocollective.com/1/2/76242/11903514/SaraAndreasson_FlamboyantBoys2_web1_670.jpg",
+        "http://payload487.cargocollective.com/1/2/76242/12041327/SaraAndreasson_LennyLetter_JillionPotter_Final_01_670.jpg",
+        "http://payload302.cargocollective.com/1/2/76242/8342965/SaraAndreasson_Vernissage4.jpg",
+        "http://payload302.cargocollective.com/1/2/76242/8342965/SaraAndreasson_Stripes.jpg",
+        "http://payload302.cargocollective.com/1/2/76242/8342965/SaraAndreasson_mickeymouse5.jpg",
+        "http://payload302.cargocollective.com/1/2/76242/8342965/SaraAndreasson_Catman2_web.jpg",
+        "http://payload302.cargocollective.com/1/2/76242/8342965/SaraAndreasson_Rasterize.jpg",
+        "http://payload328.cargocollective.com/1/2/76242/8861466/SaraAndreasson_cigarette_670.jpg",
+        "http://payload328.cargocollective.com/1/2/76242/8861466/SaraAndreasson_Girls_II_670.jpg",
+        "http://payload328.cargocollective.com/1/2/76242/8861466/SaraAndreasson_Frida_web_color_670.jpg",
+        "http://payload294.cargocollective.com/1/2/76242/8187705/SaraAndreasson_Taco_updated.jpg",
+        "http://payload294.cargocollective.com/1/2/76242/8187705/SaraAndreasson_tacotruck.jpg"]
+    },
+    {       "artist" : "Amit Shimoni",
+            "link": "http://www.shimoni-illustration.com/",
+            "description": "Amit Shimoni is a young Israeli designer, illustrator and artist. His work engage with the gaps between reality and imagination. The project HIPSTORY featured here is a series of drawings that attempt to create new and altered portrait of iconic figures of the 20th century, by placing them in different time and culture.",
+            "art" : ["http://cdn.shopify.com/s/files/1/0895/2844/products/lenin_grande.jpg?v=1434721597",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/mlk_grande.jpg?v=1434889207",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/Trump_-_HIPSTORY_-_Amit_Shimoni_grande.jpg?v=1459857088",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/obama_grande.jpg?v=1434877005",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/churchill_grande.jpg?v=1434716339",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/lama_grande.jpg?v=1434719155",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/mandella_grande.jpg?v=1434719438",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/reagan_grande.jpg?v=1434889117",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/Beatrix_grande.jpg?v=1438097636",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/gandhi_grande.jpg?v=1434397464",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/putin_grande.jpg?v=1468418630",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/de_gaulle_grande.jpg?v=1436789874",
+              "http://freeyork.org/wp-content/uploads/2015/01/hipstory10.jpg",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/che_grande.jpg?v=1434721315",
+              "http://www.mouv.fr/sites/default/files/2014/12/15/182544/mao__700.jpg",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/washington_grande.jpg?v=1434877070",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/lincoln_grande.jpg?v=1455202340",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/hillary_grande.jpg?v=1434721556",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/kennedy_grande.jpg?v=1434719011",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/abby_tlv2_grande.jpg?v=1449582934",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/hipstory_santa_grande.jpg?v=1448476196",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/golda_grande.jpg?v=1434902839",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/margaret_thatcher_grande.jpg?v=1434721473",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/queen_elizabeth_grande.jpg?v=1434719302",
+              "http://cdn.shopify.com/s/files/1/0895/2844/products/angela_merkel_grande.jpg?v=1455203097"]
+    },
+    {       "artist" : "Sacha Golberger",
+            "link": "http://sachagoldberger.com/portfolio/",
+            "description": "Sacha Goldberger is a French photographer. He has been working on the Mamika séries since 2007. Since 2008 he has exhibited in Paris at the following galleries: Wanted(2010), Bailly Contemporain (2011), Acte 2 Galerie (2012).<br> He has also exhibited at CCB Museum of Rio, New Delhi Photo Festival, Amsterdam Art Fair, London Art Fair, Angkor Festival, Getxophoto, Palm Beach Art Fair and Berliner list,  and has contributed to outdoor exhibitions in France at Vendome, Bercy Village. Two different books have been published in Europe and the USA : « Mamika, big little grandmother « (2010) and « Mamika & co » (2012).",
+            "art" : ["http://sachagoldberger.com/portfolio/wp-content/gallery/flemish-in-the-stars/21_07_13_Super-H%C3%A9ros-Flamands-_21_darkvador_1293_05.jpg",
+              "http://static1.gamerstuff.fr/wp-content/uploads/2014/11/sacha-goldberger-super-heros-flamands-cat-woman.jpg",
+              "http://actuphoto.com/files/news_34875_0.jpg",
+              "http://sachagoldberger.com/portfolio/wp-content/gallery/flemish-in-the-stars/29_Convention_STMAXIME_TUSKEN_RIDER30358_03.jpg",
+              "http://sachagoldberger.com/portfolio/wp-content/gallery/super-flemish/SuperHerosFlamands_CatwomanFace_009.jpg",
+              "http://sachagoldberger.com/portfolio/wp-content/gallery/flemish-in-the-stars/23_Convention_STCHEWBACCA_VINCENT30302_12.jpg",
+              "http://www.lesinrocks.com/content/uploads/2014/11/15_07_13_super-hros-flamands-_16_spiderman_0827_08.jpg",
+              "http://static1.gamerstuff.fr/wp-content/uploads/2014/11/sacha-goldberger-super-heros-flamands-blanche-neige.jpg",
+              "https://wegeekgirls.files.wordpress.com/2014/11/super-flamands-series-by-sacha-goldberger-13.jpg",
+              "http://static1.gamerstuff.fr/wp-content/uploads/2014/11/sacha-goldberger-super-heros-flamands-wonderwoman.jpg",
+              "http://static1.gamerstuff.fr/wp-content/uploads/2014/11/sacha-goldberger-super-heros-flamands-alice.jpg",
+              "http://www.lense.fr/wp-content/uploads/2014/11/15_07_13_Super-H%C3%A9ros-Flamands-_03_Captain_America_0130_06.jpg",
+              "http://sachagoldberger.com/portfolio/wp-content/gallery/flemish-in-the-stars/26_Convention_STPERRINE_BOUSHH30277_04.jpg",
+              "http://www.laboiteverte.fr/wp-content/uploads/2014/11/superherosflammand-05.jpg",
+              "http://sachagoldberger.com/portfolio/wp-content/gallery/family-portrait/15_07_13_Super-H%C3%A9ros-Flamands-_doublons_1114-copie.jpg",
+              "https://finnmag.files.wordpress.com/2015/04/15_07_13_super-hc3a9ros-flamands-_10_joker_007.jpg",
+              "http://i0.wp.com/jeuxdemots.sciencesjeux.com/wp-content/uploads/2016/01/sacha-goldberger-super-heros-16-siecle-23.jpg",
+              "http://sachagoldberger.com/portfolio/wp-content/gallery/super-flemish/19_15_07_13_Super-H%C3%A9ros-Flamands-_04_Wolverine_0194_04.jpg",
+              "http://sachagoldberger.com/portfolio/wp-content/gallery/super-flemish/15_07_13_Super-H%C3%A9ros-Flamands-_14_Wonderwoman_0709_5.jpg",
+              "http://www.wipplay.com//anonymiser/312087-newvis.jpg",
+              "http://1.bp.blogspot.com/-OOBF7rXRooU/VHI8FMvTBnI/AAAAAAAAAbc/_X1hqNp1cCE/s1600/SuperHerosFlamands_Leia_Sacha.jpg"]
+    },
+    {       "artist" : "Kehinde Wiley",
+            "link": "http://kehindewiley.com/",
+            "description": "Los Angeles native and New York based visual artist, Kehinde Wiley has firmly situated himself within art history’s portrait painting tradition. As a contemporary descendent of a long line of portraitists, including Reynolds, Gainsborough, Titian, Ingres, among others, Wiley, engages the signs and visual rhetoric of the heroic, powerful, majestic and the sublime in his representation of urban, black and brown men found throughout the world.",
+            "art" : ["http://i.huffpost.com/gen/597067/thumbs/o-KANCOU-DIAOVNO_LOWRES-570.jpg?4",
+              "http://i.huffpost.com/gen/597073/thumbs/o-MARY-LITTLE-570.jpg?4",
+              "http://i.huffpost.com/gen/597070/thumbs/o-DACIACARTER_LOWRES-570.jpg?1",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/02/PA11-020Terence_Nance.jpg",
+              "http://assets.coolhunting.com/coolhunting/2014/09/large_KWHaiti-Thumb.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/DR09-004_SamuelJohnson.jpg",
+              "https://s3.amazonaws.com/files.collageplatform.com.prod/image_cache/1010x580_fit/56d5695ecfaf342a038b4568/896799abc88a7b6ff660ed849cf90679.jpeg",
+              "http://www.blackbird.vcu.edu/v6n1/gallery/ravenal_j/willem-550h.jpg",
+              "http://www.seattleartmuseum.org/AssetsExhibitions/kehinde-wiley/kehinde-wiley-venetian-ambassador-630px.jpg",
+              "http://api.ning.com/files/kAYh-M0ij9ONcmysLjWz*OOtaBYdqUPYZn93pwd2VVcd70asCrq*XmYVNKhdCDtSP6zdljLk6Hz2AHyXekuH8doruIBM6myd/KehindeWiley10.jpg",
+              "http://api.ning.com/files/kAYh-M0ij9OBRzFg29r1af4HWf8rVd87zMZKypgTKDA3wLj0xkCBDLS-QqAWCgT9sG-mtSPw4DPfb1z4VaudbSjobb4QusmQ/KehindeWiley9.jpg",
+              "http://www.kehindewiley.com/images/blacklight/ABacchanteAfterSirJoshuaReynolds.jpg",
+              "http://superselected.com/wp-content/uploads/2014/09/Kehinde-Wiley-Haiti-04.jpeg",
+              "http://www.itzcaribbean.com/wp-content/uploads/2014/04/kehinde-wiley-world-stage-jamaica.jpg",
+              "http://www.kehindewiley.com/images/misc2012/Morthyn-Brito-II.jpg",
+              "http://www.seattleartmuseum.org/AssetsExhibitions/kehinde-wiley/kehinde-wiley-st-john-630px.jpg",
+              "http://www.themodern.org/sites/default/files/el137.43-web.jpg",
+              "https://www.brooklynmuseum.org/assets/system-images/made/assets/system-images/remote/https_d1lfxha3ugu3d4.cloudfront.net/exhibitions/images/2015_Kehinde_Wiley_EL137.01_2395W_600_606.jpg",
+              "https://www.brooklynmuseum.org/assets/system-images/made/assets/system-images/remote/https_d1lfxha3ugu3d4.cloudfront.net/exhibitions/images/2015_Kehinde_Wiley_EL137.63_3082W_600_801.jpg",
+              "https://www.brooklynmuseum.org/assets/system-images/made/assets/system-images/remote/https_d1lfxha3ugu3d4.cloudfront.net/exhibitions/images/2015_Kehinde_Wiley_L2005.6_1397W_600_644.jpg",
+              "https://www.brooklynmuseum.org/assets/system-images/made/assets/system-images/remote/https_d1lfxha3ugu3d4.cloudfront.net/exhibitions/images/2015_Kehinde_Wiley_EL137.71_4000W_600_688.jpg",
+              "https://www.brooklynmuseum.org/assets/system-images/made/assets/system-images/remote/https_d1lfxha3ugu3d4.cloudfront.net/exhibitions/images/2015_Kehinde_Wiley_EL137.33_2805W_600_799.jpg",
+              "https://d1lfxha3ugu3d4.cloudfront.net/assets/system-images/remote/https_d1lfxha3ugu3d4.cloudfront.net/exhibitions/images/2015_Kehinde_Wiley_EL137.39_2797W.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-001_LaRoiALaChasse-769x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-002_TriplePortraitOfCharlesI-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-003_MissSusannaGale-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-004_AferAlbertDurersSelfPortrait-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-005_TheVirginWithTheHost-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-006_SaintGermaineOfPibrac-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-007_TheAnnunciation-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-009_TheCountessOfRocksavage-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-010_TheAnnunciation-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-011_PenitentMaryMagdalene-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-012_StFrancisInTheDesert-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-013_ABacchanteAfterSirJoshuaReynolds-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-014_ElizabethWarrenAsHebe-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-016_PortraitOfDoctorSamuelJohnson.jp-g-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-017_PortraitOfSimonGeorge-768x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PH09-018_TwoMenWithAPassageFromCicerosOnFriendship-849x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/DR10-001-Mame-Study-2010.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/DR10-004-Head-of-a-Young-Girl-Veiled-Study.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/DR10-005-Sharrod-Hosten-Study-I-2010.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/DR10-007-Ivelaw-Study-2010.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/KW-DR10-008-Sharrod-Hosten-Study-IV-2010.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/PA10-005-Equestrian-Portrait-of-King-Philip-II-after-Rubens-Michael-Jackson.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/OfficerOfTheHussars-998x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/03/PA05-037_Napoleon_Leading_the_Army_Over_the_Alps-953x1024.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/DR09-004_SamuelJohnson.jpg",
+              "http://kehindewiley.com/wp/wp-content/uploads/2014/01/DR09-010-PenitentMaryMagdalen.jpg"]
+    }
+];
+
 // Init XHR Object and retrieve list from a github repo
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://raw.githubusercontent.com/askz/adblocker-art-facebook/master/current-art.json", true);
+xhr.open("GET", "https://raw.githubusercontent.com/nicolrx/adblocker-art-facebook/master/current-art.json", true);
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
     // JSON.parse does not evaluate the attacker's scripts.
     var resp = JSON.parse(xhr.responseText);
-    console.log(resp);
-    // call function
-    addArt(resp);
+    // first call function: we choose one artist and fill the popup with info
+    var random_artist = getRandomInt(0,img.length);
+    addArt(img[random_artist]);
+
     window.onload = function () {
       window.onscroll = function () {
         if (document.body.scrollTop > 30) {
-          addArt(resp);
+          addArt(img[random_artist]);
         }
       };
     };
   }
 };
 xhr.send();
-
-// https://raw.githubusercontent.com/askz/adblocker-art-facebook/master/current-art.json
-
 
 /**
  * Returns a random integer between min (inclusive) and max (inclusive)
@@ -32,18 +171,19 @@ function getRandomInt(min, max) {
 
 // Feed Ad
 function addArt(img) {
-  var count_img = img.length;
+  var count_img = img.art.length;
   var count = document.getElementsByClassName("uiStreamSponsoredLink").length;
   for (i = 0; i < count; i++) {
-    var x = document.getElementsByClassName("uiStreamSponsoredLink")[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+    var x = document.getElementsByClassName("uiStreamSponsoredLink")[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
       if (typeof x.style.content === "undefined" || !x.style.content) {
         var random_img = getRandomInt(0,count_img);
         // x.className += "art".i;
-        x.style.content = "url("+img[random_img]+")";
+        x.style.content = "url("+img.art[random_img]+")";
         x.style.width = "100%";
+        x.style.padding = "0";
         x.style.cursor = "pointer";
         x.onclick = function () {
-          window.open('http://www.shimoni-illustration.com/?ref=www.addart.co','_blank');
+          window.open(img.link+"?ref=feedart.co",'_blank');
         }
       }
     }
@@ -54,14 +194,12 @@ function addArt(img) {
     var y = document.getElementsByClassName("ego_section")[a];
       if (typeof y.style.content === "undefined" || !y.style.content) {
         var random_img2 = getRandomInt(0,count_img);
-        // x.className += "art".i;
-        y.style.content = "url("+img[random_img2]+")";
+        y.style.content = "url("+img.art[random_img2]+")";
         y.style.width = "100%";
         y.style.cursor = "pointer";
         y.onclick = function () {
-          window.open('http://www.shimoni-illustration.com/?ref=www.addart.co','_blank');
+          window.open(img.link+"?ref=feedart.co",'_blank');
         }
       }
     }
 };
-
