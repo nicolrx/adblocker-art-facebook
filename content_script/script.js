b5553b28d427e4e9e98b0a7ae4c7444c0ed460db
nico_lrx <nicolas.leroux@sciencespo.fr>
2017-06-05 21:57:12 +0100

    First commit

--- content_script/script.js
+++ content_script/script.js
@@ -1,28 +1,24 @@
-var img = ["http://cdn.shopify.com/s/files/1/0895/2844/products/lenin_grande.jpg?v=1434721597",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/mlk_grande.jpg?v=1434889207",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/Trump_-_HIPSTORY_-_Amit_Shimoni_grande.jpg?v=1459857088",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/obama_grande.jpg?v=1434877005",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/churchill_grande.jpg?v=1434716339",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/lama_grande.jpg?v=1434719155",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/mandella_grande.jpg?v=1434719438",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/reagan_grande.jpg?v=1434889117",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/Beatrix_grande.jpg?v=1438097636",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/gandhi_grande.jpg?v=1434397464",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/putin_grande.jpg?v=1468418630",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/de_gaulle_grande.jpg?v=1436789874",
-"http://freeyork.org/wp-content/uploads/2015/01/hipstory10.jpg",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/che_grande.jpg?v=1434721315",
-"http://www.mouv.fr/sites/default/files/2014/12/15/182544/mao__700.jpg",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/washington_grande.jpg?v=1434877070",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/lincoln_grande.jpg?v=1455202340",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/hillary_grande.jpg?v=1434721556",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/kennedy_grande.jpg?v=1434719011",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/abby_tlv2_grande.jpg?v=1449582934",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/hipstory_santa_grande.jpg?v=1448476196",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/golda_grande.jpg?v=1434902839",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/margaret_thatcher_grande.jpg?v=1434721473",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/queen_elizabeth_grande.jpg?v=1434719302",
-"http://cdn.shopify.com/s/files/1/0895/2844/products/angela_merkel_grande.jpg?v=1455203097"];
+var img = ["http://sachagoldberger.com/portfolio/wp-content/gallery/flemish-in-the-stars/21_07_13_Super-H%C3%A9ros-Flamands-_21_darkvador_1293_05.jpg",
+"http://static1.gamerstuff.fr/wp-content/uploads/2014/11/sacha-goldberger-super-heros-flamands-cat-woman.jpg",
+"http://actuphoto.com/files/news_34875_0.jpg",
+"http://sachagoldberger.com/portfolio/wp-content/gallery/flemish-in-the-stars/29_Convention_STMAXIME_TUSKEN_RIDER30358_03.jpg",
+"http://sachagoldberger.com/portfolio/wp-content/gallery/super-flemish/SuperHerosFlamands_CatwomanFace_009.jpg",
+"http://sachagoldberger.com/portfolio/wp-content/gallery/flemish-in-the-stars/23_Convention_STCHEWBACCA_VINCENT30302_12.jpg",
+"http://www.lesinrocks.com/content/uploads/2014/11/15_07_13_super-hros-flamands-_16_spiderman_0827_08.jpg",
+"http://static1.gamerstuff.fr/wp-content/uploads/2014/11/sacha-goldberger-super-heros-flamands-blanche-neige.jpg",
+"https://wegeekgirls.files.wordpress.com/2014/11/super-flamands-series-by-sacha-goldberger-13.jpg",
+"http://static1.gamerstuff.fr/wp-content/uploads/2014/11/sacha-goldberger-super-heros-flamands-wonderwoman.jpg",
+"http://static1.gamerstuff.fr/wp-content/uploads/2014/11/sacha-goldberger-super-heros-flamands-alice.jpg",
+"http://www.lense.fr/wp-content/uploads/2014/11/15_07_13_Super-H%C3%A9ros-Flamands-_03_Captain_America_0130_06.jpg",
+"http://sachagoldberger.com/portfolio/wp-content/gallery/flemish-in-the-stars/26_Convention_STPERRINE_BOUSHH30277_04.jpg",
+"http://www.laboiteverte.fr/wp-content/uploads/2014/11/superherosflammand-05.jpg",
+"http://sachagoldberger.com/portfolio/wp-content/gallery/family-portrait/15_07_13_Super-H%C3%A9ros-Flamands-_doublons_1114-copie.jpg",
+"https://finnmag.files.wordpress.com/2015/04/15_07_13_super-hc3a9ros-flamands-_10_joker_007.jpg",
+"http://i0.wp.com/jeuxdemots.sciencesjeux.com/wp-content/uploads/2016/01/sacha-goldberger-super-heros-16-siecle-23.jpg",
+"http://sachagoldberger.com/portfolio/wp-content/gallery/super-flemish/19_15_07_13_Super-H%C3%A9ros-Flamands-_04_Wolverine_0194_04.jpg",
+"http://sachagoldberger.com/portfolio/wp-content/gallery/super-flemish/15_07_13_Super-H%C3%A9ros-Flamands-_14_Wonderwoman_0709_5.jpg",
+"http://www.wipplay.com//anonymiser/312087-newvis.jpg",
+"http://1.bp.blogspot.com/-OOBF7rXRooU/VHI8FMvTBnI/AAAAAAAAAbc/_X1hqNp1cCE/s1600/SuperHerosFlamands_Leia_Sacha.jpg"];
 
 var count_img = img.length;
 
@@ -46,7 +42,7 @@ function addArt() {
         x.style.width = "100%";
         x.style.cursor = "pointer";
         x.onclick = function () {
-          window.open('http://www.shimoni-illustration.com/?ref=www.addart.co','_blank');
+          window.open('http://sachagoldberger.com/portfolio/?ref=feedart.co','_blank');
         }
       }
     }
@@ -62,7 +58,7 @@ function addArt() {
         y.style.width = "100%";
         y.style.cursor = "pointer";
         y.onclick = function () {
-          window.open('http://www.shimoni-illustration.com/?ref=www.addart.co','_blank');
+          window.open('http://sachagoldberger.com/portfolio/?ref=feedart.co','_blank');
         }
       }
     }
